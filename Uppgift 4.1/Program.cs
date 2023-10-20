7af91c0629173252d8f42a7d4c29f97d4d2e6c88
sere06001 <sere06001@utb.vaxjo.se>
2023-10-20 16:33:37 +0200

    Add project files.

---
+++ Uppgift 4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 31; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 200; i > 179; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1000; i < 1401; i += 50)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
